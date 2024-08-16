92224d382343da30cb0ac2733b0d9b7f5b9e57b0
Damiano Mason <57217353+Dam-99@users.noreply.github.com>
2024-08-16 16:03:49 +0200

    Day 1 part 2: node and tree struct
    Created tree struct to use to read the line char by char and discover numbers

--- src/2023/1.rs
+++ src/2023/1.rs
@@ -6,8 +6,12 @@ fn part_1(input: String) -> Result<String, String> {
     // println!("{}", input);
     let n_lines = input.lines().count();
     let mut calibration_codes = vec![-1; n_lines];
-    for (i, line) in input.lines().enumerate() { // as an improvement, it can be parallelized
-        let digits = line.chars().filter(|c| c.is_ascii_digit()).collect::<Vec<_>>();
+    for (i, line) in input.lines().enumerate() {
+        // as an improvement, it can be parallelized
+        let digits = line
+            .chars()
+            .filter(|c| c.is_ascii_digit())
+            .collect::<Vec<_>>();
         let mut number = digits.first().unwrap().to_string();
         number.push_str(&digits.last().unwrap().to_string());
         let number = number.parse::<i32>().unwrap();
@@ -20,6 +24,66 @@ fn part_1(input: String) -> Result<String, String> {
     Ok(result.to_string())
 }
 
+#[derive(Clone)]
+struct Node<T: Clone> {
+    value: T,
+    children: Vec<Node<T>>,
+}
+impl<T: Clone> Node<T> {
+    fn new(el: T) -> Node<T> {
+        let node = Node::<T> {
+            value: el,
+            children: Vec::<Node<T>>::new(),
+        };
+        node
+    }
+
+    fn add_child(mut self, child: NodeOrT<T>) -> Self {
+        let ch = match child {
+            NodeOrT::N(n) => n,
+            NodeOrT::T(el) => Node::new(el)
+        };
+        self.children.push(ch);
+        self
+    }
+
+    fn add_children(mut self, new_child: Vec<NodeOrT<T>>) -> Self {
+        for ch in new_child.iter() {
+            self.children.push(match ch { NodeOrT::N(c) => c.clone(), NodeOrT::T(val) => Node::new(val.clone()) });
+        }
+        self
+    }
+
+    fn has_children(&self) -> bool {
+        self.children.len() > 0
+    }
+}
+enum NodeOrT<T: Clone> {
+    N(Node<T>), T(T)
+}
+
+struct Tree<T: Clone> {
+    root: Option<Node<T>>,
+}
+
+impl<T: Clone> Tree<T> {
+    fn new(node: Option<Node<T>>) -> Tree<T> {
+        Tree::<T> { root: node }
+    }
+}
+
+enum Numbers {
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+}
 fn part_2(input: String) -> Result<String, String> {
     let n_lines = input.lines().count();
     let mut calibration_codes = vec![-1; n_lines];
