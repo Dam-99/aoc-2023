97115c99c397a42082abf4ef5e2e2808e1c0b796
Damiano Mason <57217353+Dam-99@users.noreply.github.com>
2024-12-02 20:08:20 +0100

    Day 4 part 2
    also clippy refactor (and typo i'm stupid)

--- src/2023/4.rs
+++ src/2023/4.rs
@@ -3,7 +3,7 @@ use std::collections::HashSet;
 pub fn run(input: String) -> Result<String, String> {
     println!("Part 1: {}", part_1(&input).unwrap());
     println!("Part 2: {}", part_2(input).unwrap());
-    Ok("Day 3 completed".to_string())
+    Ok("Day 4 completed".to_string())
 }
 
 fn part_1(input: &str) -> Result<String, String> {
@@ -15,8 +15,8 @@ fn part_1(input: &str) -> Result<String, String> {
         let mut split = lottery_nums.split('|');
         let winning = split.next().unwrap();
         let played = split.next().unwrap();
-        let mut winning_set = winning.split(' ').filter(|x| *x != "").collect::<HashSet<_>>();
-        let mut played_set = played.split(' ').filter(|x| *x != "").collect::<HashSet<_>>();
+        let winning_set = winning.split(' ').filter(|x| !x.is_empty()).collect::<HashSet<_>>();
+        let played_set = played.split(' ').filter(|x| !x.is_empty()).collect::<HashSet<_>>();
         let mut count: u32 = 0;
         for plyd in played_set {
             if winning_set.contains(plyd) {
@@ -30,7 +30,32 @@ fn part_1(input: &str) -> Result<String, String> {
 }
 
 fn part_2(input: String) -> Result<String, String> {
-    Ok("not implemented".to_string())
+    let mut copies = vec![1_u32; input.lines().count()];
+    for (i, l) in input.lines().enumerate() {
+        // copies[i] += 1; // add 1 for the original (initialized at 0)
+        let mut l_split = l.split(':');
+        l_split.next(); // skip line header
+        let lottery_nums = l_split.next().unwrap().trim();
+        let mut split = lottery_nums.split('|');
+        let winning = split.next().unwrap();
+        let played = split.next().unwrap();
+        let winning_set = winning.split(' ').filter(|x| !x.is_empty()).collect::<HashSet<_>>();
+        let played_set = played.split(' ').filter(|x| !x.is_empty()).collect::<HashSet<_>>();
+        let mut count: u32 = 0;
+        for plyd in played_set {
+            if winning_set.contains(plyd) {
+                count += 1
+            }
+        }
+        for j in 1..=count {
+            copies[i+j as usize] += copies[i]; // 1 * copies[i]
+        }
+        // println!("copies at line {i} {copies:?}");
+    }
+    let result: u32 = copies.iter().sum();
+    // println!("copies {copies:?}");
+    // println!("result {result}");
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -48,5 +73,18 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 "
         .to_string();
         assert_eq!(part_1(&input), Ok("13".to_string()))
+    }
+
+    #[test]
+    fn example_2() {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"
+        .to_string();
+        assert_eq!(part_2(input), Ok("30".to_string()))
     }
 }
